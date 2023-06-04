34108eec1160f0fe71d0426c5b17f1242d12029d
Akinbande Gbenga <olugbengalium@gmail.com>
2023-06-04 01:59:25 +0100

    Initial Commit

---
+++ HarpyBanker/HarpyBankerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarpyBankerApp.swift
+//  HarpyBanker
+//
+//  Created by Gbenga Abayomi on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HarpyBankerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
